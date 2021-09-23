c4c09304428d6edc54eee910140991c4afb36fc9
Muhammet <mohammad.nadom98@gmail.com>
2021-09-23 11:40:02 +0300

    enabled mysql remote db, added and enabled .env

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+require('dotenv').config();
+
 const dummyData = require('./dummydata');
 const app = express();
 
 app.use(express.json());
 
-app.get('/messages', (req, res) => {
+app.get('/api/messages', (req, res) => {
     console.log('working');
     return res.json(dummyData);
 });
